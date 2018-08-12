d7bf3754a5f579e418ca869544975b15ce4e0c5b
Varis Darasirikul <varis.dara@gmail.com>
2018-08-13 01:46:15 +0800

    add spy tracking

---
+++ spec/spy/tracking/spies_tracking_properties.spec.js
@@ -0,0 +1,115 @@
+/*
+Scenario - 1: The.calls.any() property should return false if the spy is not called at all
+Scenario - 2: The.calls.any() property should return true if the spy is called once
+Scenario - 3: The.calls.count() property should track the number of times the spy is called
+Scenario - 4: The.calls.argsFor(index) property should return the argument(s) corresponding to each call
+Scenario - 5: The.calls.allArgs() property should return the arguments to all calls
+Scenario - 6: The.calls.mostRecent() property should return the context(the this keyword) and arguments for the most recent call
+Scenario - 7: The.calls.first() property should return the context(the this keyword) and arguments for the first call
+Scenario - 8: The.calls.reset() property should clear all tracking for a spy
+*/
+
+describe("Jasmine Spies: ", function() {
+    describe("Tracking Properties, ", function() {
+
+        beforeEach(function() {
+            this.testPersonCriteria = new Person();
+            spyOn(this.testPersonCriteria, "getAge");
+            spyOn(this.testPersonCriteria, "checkHIV");
+        });
+
+        // Scenario 1 and 2
+        describe(".calls.any() property ", function() {
+            it("should return 'false' if spy is not called at all", function() {
+                expect(this.testPersonCriteria.getAge.calls.any())
+                    .toEqual(false);
+            });
+
+            it("should return 'true' if spy is called once", function() {
+                this.testPersonCriteria.ValidateAge("10/25/1990");
+                expect(this.testPersonCriteria.getAge.calls.any())
+                    .toEqual(true);
+            });
+        });
+
+        // Scenario 3
+        describe(".calls.count() property ", function() {
+            it("should track the number of times " +
+                "the spy is called", function() {
+                expect(this.testPersonCriteria.getAge.calls.count()).toEqual(0);
+                this.testPersonCriteria.ValidateAge("10/25/1990");
+                this.testPersonCriteria.ValidateAge("10/25/1990");
+                expect(this.testPersonCriteria.getAge.calls.count()).toEqual(2);
+            });
+        });
+
+        // Scenario 4
+        describe(".calls.argsFor(index) property ", function() {
+            it("should return the argument(s) " + "corresponding to each call", function() {
+                this.testPersonCriteria.ValidateHIV("Name1", "10/25/1990", "B+");
+                this.testPersonCriteria.ValidateHIV("Name2", "10/25/1990", "B+");
+                this.testPersonCriteria.ValidateHIV("Name3", "10/25/1990", "B+");
+                expect(this.testPersonCriteria.checkHIV.calls.argsFor(0)).toEqual(["Name1", "10/25/1990", "B+"]);
+                expect(this.testPersonCriteria.checkHIV.calls.argsFor(1)).toEqual(["Name2", "10/25/1990", "B+"]);
+                expect(this.testPersonCriteria.checkHIV.calls.argsFor(2)).toEqual(["Name3", "10/25/1990", "B+"]);
+            });
+        });
+
+        // Scenario 5
+        describe(".calls.allArgs() property ", function() {
+            it("should return the arguments " + "for all calls", function() {
+                this.testPersonCriteria.ValidateHIV("Name1", "10/25/1990", "B+");
+                this.testPersonCriteria.ValidateHIV("Name2", "10/25/1990", "B+");
+                this.testPersonCriteria.ValidateHIV("Name3", "10/25/1990", "B+");
+                expect(this.testPersonCriteria.checkHIV.calls.allArgs()).toEqual([
+                    ["Name1", "10/25/1990", "B+"],
+                    ["Name2", "10/25/1990", "B+"],
+                    ["Name3", "10/25/1990", "B+"]
+                ]);
+            });
+        });
+
+        // Scenario 6
+        describe(".calls.mostRecent() property ", function() {
+            it("should return the context (the this) and " +
+                "arguments for the most recent call", function() {
+                this.testPersonCriteria.ValidateHIV("Name1", "10/25/1990", "B+");
+                this.testPersonCriteria.ValidateHIV("Name2", "10/25/1990", "B+");
+                console.log(this.testPersonCriteria.checkHIV.calls.mostRecent());
+                expect(this.testPersonCriteria.checkHIV.calls.mostRecent()).toEqual({
+                    object: this.testPersonCriteria,
+                    args: ["Name2", "10/25/1990", "B+"],
+                    returnValue: undefined,
+                    invocationOrder: jasmine.any(Number)
+                });
+                expect(this.testPersonCriteria.checkHIV.calls.mostRecent().object).toBe(this.testPersonCriteria);
+            });
+        });
+
+        // Scenario 7
+        describe(".calls.first() property ", function() {
+            it("should return the context (the this) " + "and arguments for the first call", function() {
+                this.testPersonCriteria.ValidateAge("10/25/1990");
+                this.testPersonCriteria.ValidateAge("11/20/1988");
+                expect(this.testPersonCriteria.getAge.calls.first()).toEqual({
+                    object: this.testPersonCriteria,
+                    args: ["10/25/1990"],
+                    returnValue: undefined,
+                    invocationOrder: jasmine.any(Number)
+                });
+                expect(this.testPersonCriteria.getAge.calls.first().object).toBe(this.testPersonCriteria);
+            });
+        });
+
+        // Scenario 8
+        describe(".calls.reset() property ", function() {
+            it("should clear all " +
+                "tracking for a spy", function() {
+                this.testPersonCriteria.ValidateAge("10/25/1990");
+                expect(this.testPersonCriteria.getAge.calls.any()).toEqual(true);
+                this.testPersonCriteria.getAge.calls.reset();
+                expect(this.testPersonCriteria.getAge.calls.any()).toBe(false);
+            });
+        });
+    });
+});
